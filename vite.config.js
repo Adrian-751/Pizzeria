c81f2361c3cc8c331e71853ce386f434c7ae49ee
Adrian-751 <rodriguezadrian636@gmail.com>
2025-08-29 19:57:29 -0300

    Configure Vite and Vercel for multi-page HTML site

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        menu: './menu.html'
+      }
+    }
+  },
+  server: {
+    historyApiFallback: true
+  }
 });
